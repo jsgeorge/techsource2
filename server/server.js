8d616bd6dbe35ec728a08b1738558c5af6d84b62
George M <gmeyer1000@yahoo.com>
2019-03-19 11:43:37 -0700

    added heroku build

--- server/server.js
+++ server/server.js
@@ -2,6 +2,16 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.static("client/build"));
+
+//default
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.get("/*", (req, res) => {
+    res.sendfile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
